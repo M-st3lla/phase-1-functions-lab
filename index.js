174c43633a784978fbab56aeca83c22864e8a076
its-mst <stellaoluoch231@gmail.com>
2024-03-25 12:32:44 -0400

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file!
+//  function distanceFromHqInBlocks(pickupLocation){
+//     const headquartersLocation=42;
+//     return Math.abs(43-headquartersLocation);
+//  }
+
+
+//   function distanceFromHqInBlocks(pickupLocation){
+//      const headquartersLocation=42;
+//      return Math.abs(50-headquartersLocation);
+//   }
+
+//  function distanceFromHqInFeet (pickupLocation){
+//     const headquartersLocation=42;
+//     return Math.abs((43-headquartersLocation)*264);
+//  }
+
+//  function distanceFromHqInFeet (pickupLocation){
+//     const headquartersLocation=42;
+//     return Math.abs((50-headquartersLocation)*264);
+//  }
+
